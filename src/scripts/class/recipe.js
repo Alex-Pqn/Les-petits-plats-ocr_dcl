3a17b1e8b85db8c2cccfe90946fc23a5929b6183
Alexandre P <@paquienalexandre0@gmail.com>
2022-05-06 09:24:11 +0200

    recipe class

---
+++ src/scripts/class/recipe.js
@@ -0,0 +1,29 @@
+
+// Recipe
+export class Recipe {
+    constructor (recipe) {
+      this._name = recipe.name;
+      this._description = recipe.description;
+      this._time = recipe.time;
+      this._servings = recipe.servings;
+      this._ingredients = recipe.ingredients
+      this._utensils = recipe.ustensils
+      this._appliance = recipe.appliance
+
+      this._newUtensils = []
+      this._newIngredients = []
+    }
+
+    get newUtensils() {
+      this._newUtensils = this._utensils.map(utensil => utensil.toLowerCase())
+      return this._newUtensils
+    }
+    get newIngredients() {
+      this._ingredients.forEach(ingredient => this._newIngredients.push(ingredient.ingredient.toLowerCase()));
+      return this._newIngredients
+    }
+    get newAppliance() {
+      return this._appliance.toLowerCase()
+    }
+}
+  
