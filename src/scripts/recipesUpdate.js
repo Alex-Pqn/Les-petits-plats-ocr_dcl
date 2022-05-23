5044968019d95a9957606e2dda9c11a8697e5281
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-05-23 06:13:52 +0200

    reloadRecipes & updateRecipes logic

--- src/scripts/recipesUpdate.js
+++ src/scripts/recipesUpdate.js
@@ -1,4 +1,5 @@
 import { utensilTags, applianceTags, ingredientTags } from '../scripts/tags'
+import { recipeTemplate } from '../scripts/templates/recipe'
 import { recipes } from '../scripts/recipes'
 
 const searchInput = document.getElementById('search-input')
@@ -11,30 +12,62 @@ searchInput.addEventListener('input', (event) => {
   updateRecipes()
 })
 
-function resetRecipes () {
-	activeRecipes = []
+function reloadRecipes () {
+  const recipesSection = document.querySelector('.recipes-items');
+  const recipeItems = document.querySelectorAll('.recipes-item');
+  
+  recipeItems.forEach(recipe => recipe.remove())
+
+	activeRecipes.forEach(recipe => {
+    console.log(recipe)
+		const recipeCardDOM = recipeTemplate(recipe);
+		recipesSection.insertAdjacentHTML('beforeend', recipeCardDOM);
+	});
 }
 
 export function updateRecipes () {
   const isSearchFilterActive = () => searchFilter.length >= 3
-	const isUtensilFilterActive = () => utensilTags.length >= 1
-	const isApplianceFilterActive = () => applianceTags.length >= 1
-	const isIngredientFilterActive = () => ingredientTags.length >= 1
+	const isUtensilsFilterActive = () => utensilTags.length >= 1
+	const isAppliancesFilterActive = () => applianceTags.length >= 1
+	const isIngredientsFilterActive = () => ingredientTags.length >= 1
   
-  if (isSearchFilterActive() ||isUtensilFilterActive() || isApplianceFilterActive() || isIngredientFilterActive()) { 
-    
-    const newActiveRecipes = activeRecipes.filter(function (recipe) {
-      if (isSearchFilterActive) {
-        if (recipe._newIngredients.some(ingredient => searchFilter.includes(ingredient))) return true
-        if (recipe.newName.includes(searchFilter)) return true
-        if (recipe.newDescription.includes(searchFilter)) return true 
+  if (isSearchFilterActive() || isUtensilsFilterActive() || isAppliancesFilterActive() || isIngredientsFilterActive()) {
+    const newActiveRecipes = recipes.filter(function (recipe) {
+      let ingredientStep = false
+      let applianceStep = false
+      let utensilStep = false
+      let searchStep = false
+      
+      // ingredients filter
+      if (isIngredientsFilterActive() && (ingredientTags.every(ingredient => recipe._newIngredients.includes(ingredient)))) ingredientStep = true
+      // appliances filter
+      if (isAppliancesFilterActive() && (applianceTags.every(appliance => recipe._newAppliance.includes(appliance)))) applianceStep = true
+      // utensils filter
+      if (isUtensilsFilterActive() && (utensilTags.every(utensil => recipe._newUtensils.includes(utensil)))) utensilStep = true
+      
+      // search filters
+      if (isSearchFilterActive()) {
+        // ingredients
+        if (recipe._newIngredients.some(ingredient => searchFilter.includes(ingredient))) searchStep = true
+        // name
+        if (recipe.newName.includes(searchFilter)) searchStep = true
+        // description
+        if (recipe.newDescription.includes(searchFilter)) searchStep = true 
       }
+      
+      // steps validation
+      if (
+        (!isIngredientsFilterActive() || (isIngredientsFilterActive() && ingredientStep)) && 
+        ((!isAppliancesFilterActive() || isAppliancesFilterActive() && applianceStep)) && 
+        (!isUtensilsFilterActive() || (isUtensilsFilterActive() && utensilStep)) &&
+        (!isSearchFilterActive() || (isSearchFilterActive() && searchStep))
+        ) return true
     })
-    
-    console.log(newActiveRecipes)
-    
-    console.log(utensilTags)
-    console.log(applianceTags)
-    console.log(ingredientTags)
+    activeRecipes = newActiveRecipes
+    reloadRecipes()
+  }
+  else if (!isSearchFilterActive() && !isUtensilsFilterActive() && !isAppliancesFilterActive() && !isIngredientsFilterActive()) {
+    activeRecipes = recipes
+    reloadRecipes()
   }
 }
