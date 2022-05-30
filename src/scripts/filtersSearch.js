de26c4e71fd7ad4786015241851bcb5c52f66bbe
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-05-30 10:46:48 +0200

    Set error fix, JSDocs

--- src/scripts/filtersSearch.js
+++ src/scripts/filtersSearch.js
@@ -1,72 +1,90 @@
-import { utensilFilters, applianceFilters, ingredientFilters, activeUtensilFilters, activeApplianceFilters, activeIngredientFilters, resetApplianceFilters, resetUtensilFilters, resetIngredientFilters } from '../scripts/filters'
+import { utensilFilters, applianceFilters, ingredientFilters } from '../scripts/filters'
 import { insertAndUpdateApplianceFilters, insertAndUpdateUtensilFilters, insertAndUpdateIngredientFilters } from '../scripts/filters'
+import { getNormalizedString } from '../scripts/global'
+
+const applianceFilterItems = document.getElementsByClassName('appliance-filter-item')
+const utensilFilterItems = document.getElementsByClassName('utensil-filter-item')
+const ingredientFilterItems = document.getElementsByClassName('ingredient-filter-item')
 
 export function updateIngredientFiltersModal (searchInput) {
-  const ingredientFiltersArray = Array.from(ingredientFilters)
+  // reset ingredient filters
+  insertAndUpdateIngredientFilters()
   
   if (searchInput) {
     // filter ingredients by search input
-    const ingredientMatches = ingredientFiltersArray.filter(ingredientFilter => ingredientFilter.includes(searchInput))
+    const ingredientMatches = ingredientFilters.filter(ingredientFilter => ingredientFilter.includes(searchInput))
     
     // some ingredients match with search
     if (ingredientMatches.length >= 1) {
-      ingredientMatches.forEach(ingredientMatch => {
-        // push ingredient filter into active ingredient filters
-        activeIngredientFilters.push(ingredientMatch)
-      })
-      insertAndUpdateIngredientFilters()
+      for (let i = ingredientFilterItems.length; i >= 0; i--) {
+        if (ingredientFilterItems[i]) {
+          let isIngredientMatch
+        
+          ingredientMatches.forEach((ingredientMatch, key, ingredientMatches) => {
+            // ingredient match with filter
+            if (ingredientMatch === getNormalizedString(ingredientFilterItems[i].textContent)) isIngredientMatch = true
+            
+            // last iteration & ingredient doesn't match with any filter
+            if (Object.is(ingredientMatches.length - 1, key) && !isIngredientMatch) ingredientFilterItems[i].remove()
+          })
+        }
+      } 
     }
-    else insertAndUpdateIngredientFilters()
-  }
-  else {
-    resetIngredientFilters()
-    insertAndUpdateIngredientFilters()
+    else for (let i = ingredientFilterItems.length; i > -1; i--) if (ingredientFilterItems[i]) ingredientFilterItems[i].remove()
   }
 } 
-
 export function updateUtensilFiltersModal (searchInput) {
-  const utensilFiltersArray = Array.from(utensilFilters)
+  // reset utensil filters
+  insertAndUpdateUtensilFilters()
   
   if (searchInput) {
     // filter utensils by search input
-    const utensilMatches = utensilFiltersArray.filter(utensilFilter => utensilFilter.includes(searchInput))
+    const utensilMatches = utensilFilters.filter(utensilFilter => utensilFilter.includes(searchInput))
     
     // some utensils match with search
     if (utensilMatches.length >= 1) {
-      utensilMatches.forEach(utensilMatch => {
-        // push utensil filter into active utensil filters
-        activeUtensilFilters.push(utensilMatch)
-      })
-      insertAndUpdateUtensilFilters()
+      for (let i = utensilFilterItems.length; i > -1; i--) {
+        if (utensilFilterItems[i]) {
+          let isUtensilMatch
+        
+          utensilMatches.forEach((utensilMatch, key, utensilMatches) => {
+            // utensil match with filter
+            if (utensilMatch === getNormalizedString(utensilFilterItems[i].textContent)) isUtensilMatch = true
+            
+            // last iteration & utensil doesn't match with any filter
+            if (Object.is(utensilMatches.length - 1, key) && !isUtensilMatch) utensilFilterItems[i].remove()
+          })
+        }
+      } 
     }
-    else insertAndUpdateUtensilFilters()
-  }
-  else {
-    resetUtensilFilters()
-    insertAndUpdateUtensilFilters()
+    else for (let i = utensilFilterItems.length; i > -1; i--) if (utensilFilterItems[i]) utensilFilterItems[i].remove()
   }
 } 
-
 export function updateApplianceFiltersModal (searchInput) {
-  const applianceFiltersArray = Array.from(applianceFilters)
+  // reset appliance filters
+  insertAndUpdateApplianceFilters()
   
   if (searchInput) {
     // filter appliances by search input
-    const applianceMatches = applianceFiltersArray.filter(applianceFilter => applianceFilter.includes(searchInput))
+    const applianceMatches = applianceFilters.filter(applianceFilter => applianceFilter.includes(searchInput))
     
     // some appliances match with search
     if (applianceMatches.length >= 1) {
-      applianceMatches.forEach(applianceMatch => {
-        // push appliance filter into active appliance filters
-        activeApplianceFilters.push(applianceMatch)
-      })
-      insertAndUpdateApplianceFilters()
+      for (let i = applianceFilterItems.length; i > -1; i--) {
+        if (applianceFilterItems[i]) {
+          let isApplianceMatch
+        
+          applianceMatches.forEach((applianceMatch, key, applianceMatches) => {
+            // appliance match with filter
+            if (applianceMatch === getNormalizedString(applianceFilterItems[i].textContent)) isApplianceMatch = true
+            
+            // last iteration & appliance doesn't match with any filter
+            if (Object.is(applianceMatches.length - 1, key) && !isApplianceMatch) applianceFilterItems[i].remove()
+          })
+        }
+      } 
     }
-    else insertAndUpdateApplianceFilters()
-  }
-  else {
-    resetApplianceFilters()
-    insertAndUpdateApplianceFilters()
+    else for (let i = applianceFilterItems.length; i > -1; i--) if (applianceFilterItems[i]) applianceFilterItems[i].remove()
   }
 } 
 
--- src/scripts/recipesUpdate.js
+++ src/scripts/recipesUpdate.js
@@ -27,8 +27,12 @@ function reloadRecipes () {
 		recipesSection.insertAdjacentHTML('beforeend', recipeCardDOM);
 	});
 }
+
 /**
- * JSDOC ICI
+ * @param {String} searchFilter
+ * @param {Array} utensilTags
+ * @param {Array} applianceTags
+ * @param {Array} ingredientTags
  */
 export function updateRecipes () {
   const isSearchFilterActive = () => searchFilter.length >= 3
@@ -37,6 +41,35 @@ export function updateRecipes () {
 	const isIngredientsFilterActive = () => ingredientTags.length >= 1
   
   if (isSearchFilterActive() || isUtensilsFilterActive() || isAppliancesFilterActive() || isIngredientsFilterActive()) {
+    const newActiveRecipes = recipes.filter(function (recipe) {
+     
+      // filterStep return true if : there is minimum 1 filter && recipe contains all the filters
+      const ingredientStep = isIngredientsFilterActive() && (ingredientTags.every(ingredient => recipe._newIngredients.includes(ingredient)))
+      const applianceStep = isAppliancesFilterActive() && (applianceTags.every(appliance => recipe._newAppliance.includes(appliance)))
+      const utensilStep = isUtensilsFilterActive() && (utensilTags.every(utensil => recipe._newUtensils.includes(utensil)))
+      
+      let searchStep = false
+
+      if (isSearchFilterActive()) {
+        // ingredients
+        // searchStep return true if recipe ingredients contains minimum one ingredient
+        if (recipe._newIngredients.some(ingredient => searchFilter.includes(ingredient))) searchStep = true
+        // name/title
+        // searchStep return true if recipe name match with search filter
+        if (recipe.newName.includes(searchFilter)) searchStep = true
+        // description
+        // searchStep return true if recipe description match with search filter
+        if (recipe.newDescription.includes(searchFilter)) searchStep = true 
+      }
+      
+      // steps validation
+      if (
+        (!isIngredientsFilterActive() || ingredientStep) && 
+        (!isAppliancesFilterActive() || applianceStep) && 
+        (!isUtensilsFilterActive() || utensilStep) &&
+        (!isSearchFilterActive() || searchStep)
+        ) return true
+    })
     // push active recipes
     activeRecipes = newActiveRecipes
     reloadRecipes()
